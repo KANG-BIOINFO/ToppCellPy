a547a55fe4161605b7d1ae02c10718ad7e737089
Kang Jin <jinkg@mail.uc.edu>
2021-10-04 22:41:03 -0400

    update 1004-1

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name = "ToppCellPy",
+    version = "0.1.0",
+    description = "This package is used for ToppCell gene module generation and downstream analysis.",
+    author = "Kang Jin",
+    author_email = "Kang.Jin@cchmc.org",
+    url = "https://github.com/KANG-BIOINFO/ToppCellPy",
+    packages = find_packages(),
+    install_requires = [
+        "numpy",
+        "pandas",
+        "scanpy>=1.6.0",
+        "matplotlib",
+        "seaborn",
+        "gseapy",
+        "pickle",
+        "scipy",
+        "random",
+        "pylab"
+    ]
+)
